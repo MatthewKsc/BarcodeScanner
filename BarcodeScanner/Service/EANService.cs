315912b1cc43707cb409ce317776bb3a55696a57
ksciukmateusz <ksciukmateusz@gmail.com>
2021-06-07 20:36:33 +0200

    Added EANService for now without impl

---
+++ BarcodeScanner/Service/EANService.cs
@@ -0,0 +1,24 @@
+﻿using BarcodeScanner.Interfaces;
+using BarcodeScanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeScanner.Service
+{
+    public class EANService : IBarcodeService<EAN> {
+        public bool Scan(EAN input) {
+            if(input.EANType == EANTypes.EAN13) {
+                return true;
+            }
+
+            if(input.EANType == EANTypes.EAN8) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
