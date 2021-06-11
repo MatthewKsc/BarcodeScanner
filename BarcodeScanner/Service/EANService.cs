3f0b18d9d48e7d279f17c7dd6e4e7db1a62ab784
ksciukmateusz <ksciukmateusz@gmail.com>
2021-06-11 22:53:20 +0200

    Implementation of checkControlNumber method

--- BarcodeScanner/Service/EANService.cs
+++ BarcodeScanner/Service/EANService.cs
@@ -50,7 +50,38 @@ namespace BarcodeScanner.Service
         }
 
         private bool checkControlNumber(string ean) {
-            return true;
+            char[] eanArray = ean.ToCharArray();
+            int sumOdds = 0;
+            int sumEven = 0;
+            int controlNumber;
+            bool switcher = true;
+            for(int i=eanArray.Length-2; i>=0; i--) {
+                if (switcher) {
+                    sumOdds += (int) Char.GetNumericValue(eanArray[i]);
+                    switcher = false;
+                }
+                else {
+                    sumEven += (int)Char.GetNumericValue(eanArray[i]);
+                    switcher = true;
+                }
+            }
+
+            sumOdds *= 3;
+
+            if ((sumOdds + sumEven) % 10==0) {
+                controlNumber = 0;
+            }
+            else {
+                controlNumber = 10 - ((sumOdds + sumEven) % 10);
+            }
+
+            if (controlNumber == (int) Char.GetNumericValue(eanArray[eanArray.Length - 1])) {
+                return true;
+            }
+            else {
+                return false;
+            }
+
         }
     }
 }
