950024a2a768baa0069b5277970f7922896d31bb
ksciukmateusz <ksciukmateusz@gmail.com>
2021-06-13 22:03:32 +0200

    IEANService impl

---
+++ BarcodeScanner/Interfaces/IEANService.cs
@@ -0,0 +1,14 @@
+﻿using BarcodeScanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeScanner.Interfaces
+{
+    public interface IEANService :IBarcodeService<EAN>
+    {
+        List<EAN> GetEAN();
+    }
+}
