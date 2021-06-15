ba6d768498e9f4afbe597c84aaff60a80a7cfec6
ksciukmateusz <ksciukmateusz@gmail.com>
2021-06-15 12:28:47 +0200

    Impl of EANDto and MappingProfiles

---
+++ BarcodeScanner/DTOs/EANDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeScanner.DTOs
+{
+    public class EANDto
+    {
+        public string Barcode { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ BarcodeScanner/Extensions/MappingProfiles.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BarcodeScanner.DTOs;
+using BarcodeScanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeScanner.Extensions
+{
+    public class MappingProfiles : Profile{
+        
+        public MappingProfiles() {
+            CreateMap<EAN, EANDto>()
+                .ForMember(e => e.Type, d => d.MapFrom(s => s.EANType.ToString("g")));
+
+            CreateMap<EANDto, EAN>()
+                .ForMember(e => e.EANType, d => d.MapFrom(s => Enum.Parse(typeof(EANTypes), s.Type, true)));
+        }
+
+    }
+}
